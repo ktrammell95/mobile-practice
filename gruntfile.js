be8c27905f36e964958f69ed2d2df1507af730cc
Katherine Trammell <ktrammell@gmail.com>
2015-07-11 16:08:40 -0400

    updated gruntfile

--- gruntfile.js
+++ gruntfile.js
@@ -64,7 +64,7 @@ module.exports = function(grunt) {
 
 	// 3. Where we tell Grunt we plan to use this plug-in.
 	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
+	// grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-less');
 	grunt.loadNpmTasks('grunt-contrib-watch');
